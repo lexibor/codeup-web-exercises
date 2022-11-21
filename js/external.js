7947e9bbf2f4750fc36c975d08a99b6f7505f6dc
Alexia Briones Ornelas <brionesornelas.alexia@gmail.com>
2022-11-21 13:40:24 -0600

    copied from "javascript-from-html" repository

---
+++ js/external.js
@@ -0,0 +1,7 @@
+"use strict";
+
+console.log('Hello from external JavaScript');
+alert('Welcome to my website!');
+
+let userInput = prompt('What is your favorite color?');
+alert('Great, ' + userInput + ' is my favorite color too!!');
