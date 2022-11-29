5307422e98f9d197bb8d14aee37f38785c3bc9ec
Alexia Briones Ornelas <brionesornelas.alexia@gmail.com>
2022-11-29 09:05:59 -0600

    started loops exercises

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,1 +1,15 @@
-console.log('hi');
+function showMultiplicationTable(num)
+{
+    let mult;
+    for(let i = 1; i < 11; i++)
+    {
+        mult = num * i;
+        console.log(`${num} x ${i} = ${mult}`);
+    }
+}
+showMultiplicationTable(7);
+
+//////even or odd for loop////////
+console.log('EVEN OR ODD');
+
+let random = Math.floor((Math.random() * 200) + 20);
