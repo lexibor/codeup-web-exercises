ec4fafde30cb2e686e527efae93e1004b8049140
Alexia Briones Ornelas <brionesornelas.alexia@gmail.com>
2022-11-29 11:50:33 -0600

    finished for loops of exercises

--- js/for_loops.js
+++ js/for_loops.js
@@ -10,6 +10,46 @@ function showMultiplicationTable(num)
 showMultiplicationTable(7);
 
 //////even or odd for loop////////
-console.log('EVEN OR ODD');
+console.log('');
 
-let random = Math.floor((Math.random() * 200) + 20);
+for(let i = 0; i < 10; i++)
+{
+    let random = Math.floor((Math.random() * 200) + 20);
+    console.log(`${random} is ${evenOrOdd(random)}`);
+}
+
+//console.log('');
+
+for(let i = 0; i < 10; i++)
+{
+    let numberStair = [];
+
+    for(let j = 0; j < i; j++)
+    {
+        numberStair.push(i);
+    }
+    console.log(numberStair.join(''));
+}
+
+console.log('')
+
+for(let i = 100; i > 0; i -= 5)
+{
+    console.log(i);
+}
+
+
+
+
+function evenOrOdd(num)
+{
+    if(num % 2 === 0)
+    {
+        return `Even`;
+    }
+    else
+    {
+        return `Odd`;
+    }
+
+}
