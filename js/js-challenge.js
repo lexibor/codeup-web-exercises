9909f6abe135e20899edbc3d25ad3deddfb0c636
Alexia Briones Ornelas <brionesornelas.alexia@gmail.com>
2023-01-10 09:15:30 -0600

    finished warmup

--- js/js-challenge.js
+++ js/js-challenge.js
@@ -1,4 +1,28 @@
-
+//
+// // const pets = [
+// //     {
+// //         name: 'Sparky',
+// //         type: 'Fish',
+// //         age: 4
+// //     },
+// //     {
+// //         name: 'Mr. Pig',
+// //         type: 'Cat',
+// //         age: 4
+// //     },
+// //     {
+// //         name: 'Bubba',
+// //         type: 'Dog',
+// //         age: 5
+// //     },
+// //     {
+// //         name: 'Pickles',
+// //         type: 'Dog',
+// //         age: 10
+// //     }
+// //
+// // ];
+//
 // const pets = [
 //     {
 //         name: 'Sparky',
@@ -16,184 +40,228 @@
 //         age: 5
 //     },
 //     {
-//         name: 'Pickles',
+//         name: 'Beans',
 //         type: 'Dog',
-//         age: 10
+//         age: 3
+//     },
+//     {
+//         name: 'Mr. Salmon',
+//         type: 'Fish',
+//         age: 1
+//     }
+// ];
+//
+// function findAverageDogAge(arr)
+// {
+//     let avg = 0;
+//     let count = 0;
+//
+//         for(let i = 0; i < arr.length; i++)
+//         {
+//             if(arr[i].type === 'Dog')
+//             {
+//                 avg += arr[i].age;
+//                 count++;
+//
+//             }
+//         }
+//     return avg / count;
+// }
+//
+// console.log(findAverageDogAge(pets)); // returns 7.5
+//
+//
+// function returnLongestPetName(arr)
+// {
+//     let longestName = '';
+//
+//     // for(let i = 0; i < arr.length; i++)
+//     // {
+//     //     // console.log(arr[i].name.length, arr[i].name);
+//     //     if(arr[i].name.length > longestName.length)
+//     //     {
+//     //         longestName = arr[i].name;
+//     //     }
+//     // }
+//     //
+//     arr.forEach(function(pet){
+//         if(pet.name.length > longestName.length)
+//         {
+//             longestName = pet.name;
+//         }
+//     });
+//
+//     return longestName;
+//
+// }
+// console.log(returnLongestPetName(pets)); //returns Mr. Salmon
+//
+//
+// function returnLastTwoChars(str)
+// {
+//     return str.slice(-2);
+// }
+//
+// console.log(returnLastTwoChars('hello'));
+//
+// function returnLastCharsReversed(str, num)
+// {
+//     return str.slice(-num).split('').reverse().join('');
+// }
+//
+// console.log(returnLastCharsReversed('codeup', 3));
+//
+//
+// // ================================= WARM UP
+// // Write a function, iBeforeE that takes in a string and returns the string with any ‘ei’ characters replaced with ‘ie’.
+// // iBeforeE(‘ei’) // returns ‘ie’
+// // iBeforeE(‘height’) // returns ‘hieght’
+// // iBeforeE(‘heist’) // returns ‘hiest’
+// // iBeforeE(‘their’) // returns ‘thier’
+// // iBeforeE(‘theirtheir’) // returns ‘thierthier’
+//
+//
+// function iBeforeE(str)
+// {
+//     let newStr = '';
+//     if(str.includes('ei'))
+//     {
+//         newStr =  str.replaceAll('ei', 'ie');
 //     }
 //
-// ];
+//     return newStr;
+// }
+//
+// console.log(iBeforeE('ei'));
+// console.log(iBeforeE('height'));
+// console.log(iBeforeE('theirtheir'));
+//
+// // ================================= WARM UP
+// // Create a function, filterList, that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+// // filterList([1,2,‘a’,‘b’]) // returns [1,2]
+// // filterList([1,‘a’,‘b’,0,15]) // returns [1,0,15]
+// // filterList([1,2,‘aasf’,‘1’,‘123’,123]) // returns [1,2,123]
+//
+// function filterList(arr)
+// {
+//     let numArr = [];
+//     arr.forEach(function(num){
+//         if(typeof num !== 'string' && num > 0)
+//         {
+//             numArr.push(num);
+//         }
+//     });
+//
+//     return numArr.join(', ');
+// }
+//
+// console.log(filterList([1,2,'a','b'])); // returns [1,2]
+// console.log(filterList([1,'a','b',0,15]));// returns [1,0,15]
+// console.log(filterList([1,2,'aasf','1','123',123,-1])); // returns [1,2,123]
+//
+//
+//
+//
+// // ================================= WARM UP
+// //
+// // Create a function, zipArrays, that takes two array inputs of the same length and returns an array with the elements of both arrays alternating in the order of first[0], second[0], first[1], second[1], etc. If both arrays are empty, return an empty array.
+// //
+// //
+// // zipArrays([], []) // returns []
+// // zipArrays([1], [2]) // returns [1, 2]
+// // zipArrays(['a', 'b'], ['c', 'd']) // returns... ['a', 'c', 'b', 'd']
+// // zipArrays([1,2,'a','b'], ['bob', null, 'sally', 25]) // returns...
+// //
+// //     [
+// //     1,
+// //         'bob',
+// //         2,
+// //         null,
+// //         'a',
+// //         'sally',
+// //         'b',
+// //         25
+// //     ]
+//
+// function zipArrays(arr1, arr2)
+// {
+//     let finalArr = [];
+//     for(let i = 0; i < arr1.length; i++)
+//     {
+//         finalArr.push(arr1[i], arr2[i]);
+//     }
+//     return finalArr;
+// }
+//
+//
+//
+// console.log(zipArrays(['a', 'b'], ['c', 'd'])); // returns... ['a', 'c', 'b', 'd']
+// console.log(zipArrays([1,2,'a','b'], ['bob', null, 'sally', 25])) // returns... [1, 'bob', 2, null, 'a', 'sally', 'b', 25]
 
-const pets = [
-    {
-        name: 'Sparky',
-        type: 'Fish',
-        age: 4
-    },
-    {
-        name: 'Mr. Pig',
-        type: 'Cat',
-        age: 4
-    },
-    {
-        name: 'Bubba',
-        type: 'Dog',
-        age: 5
-    },
-    {
-        name: 'Beans',
-        type: 'Dog',
-        age: 3
-    },
-    {
-        name: 'Mr. Salmon',
-        type: 'Fish',
-        age: 1
-    }
-];
+// ================================= WARM UP
+//
+// Write a function, getUserCredentials, that takes in a list of user objects and returns a list of user objects with only the username and password properties. Assume at least an array of one user object.
 
-function findAverageDogAge(arr)
+
+    const users = [
+    {
+        firstName: 'Justin',
+        lastName: 'Reich',
+        dob: '1923-01-01',
+        username: 'jreich',
+        password: '$2y$10$UJlsa5vWq5DUKJjyO38gM.dCZfudWOFCrLWQosh0mhXKaZmRmvDse'
+    },
+    {
+        firstName: 'Sally',
+        lastName: 'Smith',
+        dob: '1935-03-11',
+        username: 'ssmith',
+        password: '$2y$10$VaLGU5.7uQLr.eg6kSI9seOcP4JY4XktWt28I9JgblAGIDpkDXbya'
+    },
+    {
+        firstName: 'Fred',
+        lastName: 'Smith',
+        dob: '1999-01-21',
+        username: 'fsmith',
+        password: '$2y$10$3USt6Dl8TNMkeh0KioPnfeVpynAotXvSIJ5xrzAHragPEAWMYEBNS'
+    },
+]
+
+
+function getUserCredentials(userArr)
 {
-    let avg = 0;
-    let count = 0;
+    let objArr = [];
+    let newObj;
 
-        for(let i = 0; i < arr.length; i++)
-        {
-            if(arr[i].type === 'Dog')
-            {
-                avg += arr[i].age;
-                count++;
+    userArr.forEach(function(arr){
+        newObj = {
+            username: arr.username,
+            password: arr.password
 
-            }
         }
-    return avg / count;
-}
-
-console.log(findAverageDogAge(pets)); // returns 7.5
-
-
-function returnLongestPetName(arr)
-{
-    let longestName = '';
-
-    // for(let i = 0; i < arr.length; i++)
-    // {
-    //     // console.log(arr[i].name.length, arr[i].name);
-    //     if(arr[i].name.length > longestName.length)
-    //     {
-    //         longestName = arr[i].name;
-    //     }
-    // }
-    //
-    arr.forEach(function(pet){
-        if(pet.name.length > longestName.length)
-        {
-            longestName = pet.name;
-        }
+        objArr.push(newObj);
     });
 
-    return longestName;
-
-}
-console.log(returnLongestPetName(pets)); //returns Mr. Salmon
-
-
-function returnLastTwoChars(str)
-{
-    return str.slice(-2);
+    return objArr;
 }
 
-console.log(returnLastTwoChars('hello'));
+console.log(getUserCredentials(users)); // returns...
 
-function returnLastCharsReversed(str, num)
-{
-    return str.slice(-num).split('').reverse().join('');
-}
+/*
 
-console.log(returnLastCharsReversed('codeup', 3));
+[
+  {
+    username: 'jreich',
+    password: '$2y$10$UJlsa5vWq5DUKJjyO38gM.dCZfudWOFCrLWQosh0mhXKaZmRmvDse'
+  },
+  {
+    username: 'ssmith',
+    password: '$2y$10$VaLGU5.7uQLr.eg6kSI9seOcP4JY4XktWt28I9JgblAGIDpkDXbya'
+  },
+  {
+    username: 'fsmith',
+    password: '$2y$10$3USt6Dl8TNMkeh0KioPnfeVpynAotXvSIJ5xrzAHragPEAWMYEBNS'
+  }
+]
 
-
-// ================================= WARM UP
-// Write a function, iBeforeE that takes in a string and returns the string with any ‘ei’ characters replaced with ‘ie’.
-// iBeforeE(‘ei’) // returns ‘ie’
-// iBeforeE(‘height’) // returns ‘hieght’
-// iBeforeE(‘heist’) // returns ‘hiest’
-// iBeforeE(‘their’) // returns ‘thier’
-// iBeforeE(‘theirtheir’) // returns ‘thierthier’
-
-
-function iBeforeE(str)
-{
-    let newStr = '';
-    if(str.includes('ei'))
-    {
-        newStr =  str.replaceAll('ei', 'ie');
-    }
-
-    return newStr;
-}
-
-console.log(iBeforeE('ei'));
-console.log(iBeforeE('height'));
-console.log(iBeforeE('theirtheir'));
-
-// ================================= WARM UP
-// Create a function, filterList, that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
-// filterList([1,2,‘a’,‘b’]) // returns [1,2]
-// filterList([1,‘a’,‘b’,0,15]) // returns [1,0,15]
-// filterList([1,2,‘aasf’,‘1’,‘123’,123]) // returns [1,2,123]
-
-function filterList(arr)
-{
-    let numArr = [];
-    arr.forEach(function(num){
-        if(typeof num !== 'string' && num > 0)
-        {
-            numArr.push(num);
-        }
-    });
-
-    return numArr.join(', ');
-}
-
-console.log(filterList([1,2,'a','b'])); // returns [1,2]
-console.log(filterList([1,'a','b',0,15]));// returns [1,0,15]
-console.log(filterList([1,2,'aasf','1','123',123,-1])); // returns [1,2,123]
-
-
-
-
-// ================================= WARM UP
-//
-// Create a function, zipArrays, that takes two array inputs of the same length and returns an array with the elements of both arrays alternating in the order of first[0], second[0], first[1], second[1], etc. If both arrays are empty, return an empty array.
-//
-//
-// zipArrays([], []) // returns []
-// zipArrays([1], [2]) // returns [1, 2]
-// zipArrays(['a', 'b'], ['c', 'd']) // returns... ['a', 'c', 'b', 'd']
-// zipArrays([1,2,'a','b'], ['bob', null, 'sally', 25]) // returns...
-//
-//     [
-//     1,
-//         'bob',
-//         2,
-//         null,
-//         'a',
-//         'sally',
-//         'b',
-//         25
-//     ]
-
-function zipArrays(arr1, arr2)
-{
-    let finalArr = [];
-    for(let i = 0; i < arr1.length; i++)
-    {
-        finalArr.push(arr1[i], arr2[i]);
-    }
-    return finalArr;
-}
-
-
-
-console.log(zipArrays(['a', 'b'], ['c', 'd'])); // returns... ['a', 'c', 'b', 'd']
-console.log(zipArrays([1,2,'a','b'], ['bob', null, 'sally', 25])) // returns... [1, 'bob', 2, null, 'a', 'sally', 'b', 25]
+*/
