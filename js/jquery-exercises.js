ffee9b5613224526be587727a7bf6607da4216dd
Alexia Briones Ornelas <brionesornelas.alexia@gmail.com>
2023-01-17 11:27:53 -0600

    finished mouse event jquery exercise

--- js/jquery-exercises.js
+++ js/jquery-exercises.js
@@ -1,68 +1,42 @@
 $(function(){
     "use strict";
 
-    alert('The DOM has finished loading!');
+    // $(function() {
+    //     alert( 'The DOM has finished loading!' );
+    // });
 
+    // alert($('#welcome').html());
 
+    // $('.codeup').css({
+    //     border: '1px solid red'
+    // });
 
-    // Vanilla javascript grabbing text from an html element
-// const grabText = document.querySelector("#ChangeWithVS");
-// console.log(grabText.textContent);
+    // $('li').css('font-size', '20px');
 
-// JQuery
-// const grabContent = $("#ChangeWithVS").text();
-// console.log(grabContent);
+    // $('h1, p, li').css('background-color', 'yellow');
 
-// Vanilla javascript to change BG color
-// const changeBg = document.getElementById("changeBg");
-// changeBg.style.background = "red";
+    // alert($('h1').html());
 
-// JQuery
-// $("#changeBgJQ").css("background", "yellow");
+    $('h1').on('click', function()
+    {
+        $(this).css('background-color', 'purple')
+    })
 
-// Vanilla javascript to add an item to the DOM
-// const body = document.getElementsByTagName("body");
-// const newElem = document.createElement("p");
-// newElem.innerHTML = "Hello world I am from vanilla";
-// body[0].appendChild(newElem);
+    $('p').on('dblclick', function()
+    {
+        $(this).css('font-size', '18px')
+    })
 
-// JQuery
-// $("<p>") // creates a paragraph tag
-//   .text("Hello World I am from jquery!") // adds text to it
-//   .appendTo("#parent-div"); // appends it to the parent
-
-// Vanilla Javascript to change text on the screen
-// const changeText = document.querySelector("#ChangeWithVS");
-// changeText.innerHTML = "I Changed content with Vanilla Javascript";
-
-//JQuery
-// $("#ChangeWithJQ").html("I changed with Jquery");
-
-// Events with Vanilla
-// const button = document.querySelector("#btn");
-// button.addEventListener("click", function (e) {
-//   e.preventDefault();
-//   console.log("button clicked");
-// });
-
-// Events with jQuery
-// $("#btn").on("click", function () {
-//   console.log("button was clicked with JQuery");
-// });
-
-// jQuery way of stacking events
-// $("#btn").on({
-//   click: function () {
-//     alert("Button clicked!");
-//   },
-//   mouseenter: function () {
-//     $(this).css("background-color", "green");
-//   },
-//   mouseleave: function () {
-//     $(this).css("background-color", "");
-//   },
-// });
-
+    $('li').hover(
+        function()
+        {
+            $(this).css('color', 'red')
+        },
+        function()
+        {
+            $(this).css('color', '')
+        }
+    )
 
 
 
