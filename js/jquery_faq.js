c641972dd31165508e57ee16ae4cb886cbe4f5f1
Alexia Briones Ornelas <brionesornelas.alexia@gmail.com>
2023-01-18 16:36:56 -0600

    finished bonus

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -31,19 +31,48 @@ $('dt').on('click', function ()
         $(this).parent().children().first().toggleClass('blue-text');
     })
 
+    let images = $('#images');
 
-    $('btn-1').on('click', function()
+    $('#btn-1').on('click', function()
     {
+
+        images
+            .children()
+            .first()
+            .detach()
+            .insertAfter(images.children().first());
 
     })
 
-    $('btn-2').on('click', function()
+    $('#btn-2').on('click', function()
     {
+        let random = Math.floor(Math.random() * 100);;
 
+        if(random % 2 === 0)
+        {
+            images
+                .children()
+                .first()
+                .detach()
+                .insertAfter(images.children().first());
+        }
+        else
+        {
+            images
+                .children()
+                .last()
+                .detach()
+                .insertBefore(images.children().last());
+        }
     })
 
-    $('btn-3').on('click', function()
+    $('#btn-3').on('click', function()
     {
+        images
+            .children()
+            .last()
+            .detach()
+            .insertBefore(images.children().last());
 
     })
 
