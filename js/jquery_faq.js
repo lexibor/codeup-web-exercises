8c1b3bedae1429120a5039be42222f9a9ffffa43
Alexia Briones Ornelas <brionesornelas.alexia@gmail.com>
2023-01-18 15:11:04 -0600

    finished main traversal exercise

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -9,5 +9,44 @@ $('dt').on('click', function ()
     $(this).toggleClass('highlight');
 })
 
+    $('#highlight').on('click', function()
+    {
+        $('#fun-facts')
+            .children()
+            .children()
+            .next()
+            .each(function(i, el)
+            {
+                $(el).children().last().toggleClass('highlight');
+            })
+    })
+
+    $('h3').on('click', function()
+    {
+        $(this).nextAll('ul:first').toggleClass('bold-text')
+    })
+
+    $('li').on('click', function()
+    {
+        $(this).parent().children().first().toggleClass('blue-text');
+    })
+
+
+    $('btn-1').on('click', function()
+    {
+
+    })
+
+    $('btn-2').on('click', function()
+    {
+
+    })
+
+    $('btn-3').on('click', function()
+    {
+
+    })
+
+
 
 })
