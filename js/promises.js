b5e980f983eda2897987805fa71922def6fbfcd2
Alexia Briones Ornelas <brionesornelas.alexia@gmail.com>
2023-01-30 14:32:26 -0600

    more edits to promise exercise

--- js/promises.js
+++ js/promises.js
@@ -2,16 +2,28 @@
     "use strict";
 
 
-        try
-        {
-            let response = await fetch('https://api.github.com/users/lexibor/events', {headers: {'Authorization': `token ${keys.gitHub}`}});
-            let data = await response.json();
-            console.log(data[0].created_at);
-        }
-        catch(error)
-        {
-            console.log(error)
-        }
+    let username = prompt('Enter your github username please :)');
+
+async function userCommit(username)
+{
+    try
+    {
+        let response = await fetch(`https://api.github.com/users/${username}/events`, {headers: {'Authorization': `token ${keys.gitHub}`}});
+        let data = await response.json();
+        return(data);
+    }
+    catch(error)
+    {
+        return(error)
+    }
+}
+
+    let user = await userCommit(username);
+
+    console.log(user);
+    console.log(`Last commit was made on: ${user[0].created_at}`);
+    console.log(`Username: ${user[0].actor.login}`);
+
 
 })();
 
